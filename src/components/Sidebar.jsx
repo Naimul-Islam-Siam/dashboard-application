3ccaffe1d8fe1f2f9777480810296d931ff61256
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-05-28 09:18:38 +0600

    handleCloseSidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useStateContext } from '../contexts/ContextProvider';
 import { Link, NavLink } from "react-router-dom";
 import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
@@ -6,7 +7,13 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { links } from "./../data/dummy";
 
 const Sidebar = () => {
-   const activeMenu = true;
+   const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+   const handleCloseSidebar = () => {
+      if (activeMenu && screenSize <= 900) {
+         setActiveMenu(false);
+      }
+   };
+
    const activeLinkStyle = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
    const normalLinkStyle = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
@@ -14,12 +21,12 @@ const Sidebar = () => {
       <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
          {activeMenu && (<>
             <div className="flex justify-between items-center">
-               <Link to="/" onClick={() => { }} className="items-center gap-3 ml-3 mt-4 text-xl font-extrabold tracking-tight flex dark:text-white text-slate-900">
+               <Link to="/" onClick={handleCloseSidebar} className="items-center gap-3 ml-3 mt-4 text-xl font-extrabold tracking-tight flex dark:text-white text-slate-900">
                   <SiShopware /> <span>Shoppy</span>
                </Link>
 
                <TooltipComponent content="Menu" position="BottomCenter">
-                  <button className="text-xl block p-3 mt-4 hover:bg-light-gray rounded-full md:hidden">
+                  <button type="button" onClick={() => { setActiveMenu((prev) => !prev) }} className="text-xl block p-3 mt-4 hover:bg-light-gray rounded-full md:hidden">
                      <MdOutlineCancel />
                   </button>
                </TooltipComponent>
@@ -32,7 +39,7 @@ const Sidebar = () => {
                         {item.title}
                      </p>
                      {item.links.map((link) => (
-                        <NavLink to={`/${link.name}`} onClick={() => { }} key={link.name} className={({ isActive }) => isActive ? activeLinkStyle : normalLinkStyle}>
+                        <NavLink to={`/${link.name}`} onClick={handleCloseSidebar} key={link.name} className={({ isActive }) => isActive ? activeLinkStyle : normalLinkStyle}>
                            {link.icon}
                            <span className="capitalize">{link.name}</span>
                         </NavLink>
