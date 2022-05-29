bf468450d6fc1bb06c1ab5d1171f50398b8280c7
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-05-29 23:19:35 +0600

    StackedChart Component

--- src/components/Charts/StackedChart.jsx
+++ src/components/Charts/StackedChart.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React from "react";
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from "@syncfusion/ej2-react-charts";
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from "../../data/dummy";
 
-const StackedChart = () => {
+const StackedChart = ({ width, height }) => {
    return (
-      <div>StackedChart</div>
+      <ChartComponent
+         id="charts"
+         width={width}
+         height={height}
+         tooltip={{ enable: true }}
+         chartArea={{ border: { width: 0 } }}
+         primaryXAxis={stackedPrimaryXAxis}
+         primaryYAxis={stackedPrimaryYAxis}
+         legendSettings={{ background: "white" }}
+      >
+         <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+         <SeriesCollectionDirective>
+            {stackedCustomSeries.map((item, index) => (
+               <SeriesDirective key={index} {...item} />
+            ))}
+         </SeriesCollectionDirective>
+      </ChartComponent>
    );
 };
 
