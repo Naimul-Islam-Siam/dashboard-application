73da06551627ff5c046df0e06c1daa8914ca274a
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-06-08 21:00:50 +0600

    Editor Page

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { RichTextEditorComponent, HtmlEditor, Image, Link, Toolbar, QuickToolbar, Inject } from "@syncfusion/ej2-react-richtexteditor";
+import { Header } from "../components";
+import { EditorData } from "../data/dummy";
 
 const Editor = () => {
    return (
-      <div>Editor</div>
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 rounded-3xl bg-white">
+         <Header category="App" title="Editor" />
+         <RichTextEditorComponent>
+            <EditorData />
+            <Inject services={[HtmlEditor, Image, Link, Toolbar, QuickToolbar]} />
+         </RichTextEditorComponent>
+      </div>
    );
 };
 
