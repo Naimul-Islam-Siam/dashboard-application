aa1e1d67324f2d552a21ad3c97f863b4281fbd53
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-06-12 00:00:24 +0600

    Pie Chart Page

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Header, PieChart } from "../../components";
 
 const Pie = () => {
    return (
-      <div>Pie</div>
+      <div className="m-4 md:m-10 mt-24 p-10 rounded-3xl bg:white dark:bg-secondary-dark-bg">
+         <Header category="Pie Chart" title="Project Cost Breakdown" />
+         <div className="w-full">
+            <PieChart />
+         </div>
+      </div>
    );
 };
 
