7fa1203f79112754fb95a48039e86d05443eda67
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-06-12 00:17:38 +0600

    ColorMapping Chart Page

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Header, ColorMappingChart } from "../../components";
 
 const ColorMapping = () => {
    return (
-      <div>ColorMapping</div>
+      <div className="m-4 md:m-10 mt-24 p-10 rounded-3xl bg:white dark:bg-secondary-dark-bg" >
+         <Header category="Color Mapping Chart" title="USA CLIMATE - WEATHER BY MONTH" />
+         <div className="w-full">
+            <ColorMappingChart />
+         </div>
+      </div >
    );
 };
 
