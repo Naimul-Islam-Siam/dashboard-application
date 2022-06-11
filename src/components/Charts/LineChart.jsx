8e7a3d90c1bfd304bc512a1e5509a9bf1eaec9d7
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-06-11 23:26:29 +0600

    LineChart Component

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React from "react";
+import { ChartComponent, SeriesDirective, SeriesCollectionDirective, LineSeries, DateTime, Legend, Tooltip, Inject } from "@syncfusion/ej2-react-charts";
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
+   const { currentThemeMode } = useStateContext();
+
    return (
-      <div>LineChart</div>
+      <ChartComponent
+         id="line-chart"
+         height="420px"
+         primaryXAxis={LinePrimaryXAxis}
+         primaryYAxis={LinePrimaryYAxis}
+         chartArea={{ border: { width: 0 } }}
+         tooltip={{ enable: true }}
+         background={currentThemeMode === "Dark" ? "#33373e" : "#fff"}
+         legendSettings={{ background: 'white' }}
+      >
+         <SeriesCollectionDirective>
+            {lineCustomSeries.map((item, index) => (
+               <SeriesDirective key={index} {...item} />
+            ))}
+         </SeriesCollectionDirective>
+         <Inject services={[LineSeries, DateTime, Tooltip, Legend]} />
+      </ChartComponent>
    );
 };
 
