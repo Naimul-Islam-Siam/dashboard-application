4f63e0fe3bfbcf1f0172a36193d05f8c95275bfd
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-06-12 00:33:17 +0600

    Financial Chart Page

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Header, FinancialChart } from "../../components";
 
 const Financial = () => {
    return (
-      <div>Financial</div>
+      <div className="m-4 md:m-10 mt-24 p-10 rounded-3xl bg:white dark:bg-secondary-dark-bg">
+         <Header category="Financial Chart" title="Apple Historical" />
+         <div className="w-full">
+            <FinancialChart />
+         </div>
+      </div>
    );
 };
 
