12d1af4096a525d93fb5cca6886296393e05961a
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-06-12 00:06:07 +0600

    Stacked Chart Page

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Header, StackedChart } from "../../components";
 
 const Stacked = () => {
    return (
-      <div>Stacked</div>
+      <div className="m-4 md:m-10 mt-24 p-10 rounded-3xl bg:white dark:bg-secondary-dark-bg">
+         <Header category="Stacked Chart" title="Project Expense" />
+         <div className="w-full">
+            <StackedChart />
+         </div>
+      </div>
    );
 };
 
