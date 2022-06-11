c3e2027de3e9ffaf613fa0a1f59524483cc82dc5
Naimul Islam Siam <42283569+Naimul-Islam-Siam@users.noreply.github.com>
2022-06-11 23:37:48 +0600

    Area Chart Page

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Header, AreaChart } from "../../components";
 
 const Area = () => {
    return (
-      <div>Area</div>
+      <div className="m-4 md:m-10 mt-24 p-10 rounded-3xl bg:white dark:bg-secondary-dark-bg">
+         <Header category="Area" title="Inflation Rate in Percentage" />
+         <div className="w-full">
+            <AreaChart />
+         </div>
+      </div>
    );
 };
 
